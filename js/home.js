9143d4f05789742ea80b38b811ccee56353a0397
MD INZAMUL HAQUE JOY <inzamulhaque1002@gmail.com>
2024-09-19 16:11:41 +0600

    first commit

---
+++ js/home.js
@@ -0,0 +1,27 @@
+document.getElementById('btn-add-money')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const addMoneyInput = document.getElementById('input-add-money').value;
+        const pinNumberInput = document.getElementById('input-pin-number').value;
+
+
+
+        if (pinNumberInput === '1234') {
+            const balance = document.getElementById('account-balance').innerText;
+
+            const addMoneyNumber = parseFloat(addMoneyInput);
+            const balanceNumber = parseFloat(balance);
+
+            const newBalance = addMoneyNumber + balanceNumber ;
+
+            console.log(newBalance)
+
+            document.getElementById('account-balance').innerText = newBalance ;
+
+
+        } else {
+            alert('Failed to add Money')
+        }
+
+    });
